dfb5cbfe27534fa6ee94d8c346cb3cccade7aafb
Zohair <zohairalsheikh2002@gmail.com>
2023-05-30 01:12:07 -0500

    Fixed some things.

---
+++ server/connection/anotherfile.go
@@ -0,0 +1,9 @@
+package handler
+
+import (
+	"fmt"
+)
+
+func AnotherFunc() {
+	fmt.Println("From another function.")
+}
---
+++ server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net"
+
+	handle "global-chat-server/server/connection"
+)
+
+func main() {
+	l, err := net.Listen("tcp", ":3001")
+	if err != nil {
+		fmt.Println("Error occured connecting.")
+		return
+	}
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting request.")
+			return
+		}
+		go handle.HandleConnection(conn)
+	}
+}
